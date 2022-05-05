1e8dc6fba35506d15aeab521610aa68ce8f6ae88
Diana Gutierrez <dianacaro46@gmail.com>
2022-05-05 15:34:24 -0500

    Fisrs example with Rest Assured was add

---
+++ src/test/java/com/restassured/test/TestsExamples.java
@@ -0,0 +1,29 @@
+package com.restassured.test;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestsExamples {
+	
+	@Test
+	public void test_one() {
+		
+		int statusExpected = 200; 
+		Response response =  RestAssured.get("https://reqres.in/api/users?page=2");
+		
+		int responseStatusCode = response.getStatusCode();
+		long time = response.getTime();
+		String responseBody = response.getBody().asString();
+		
+		System.out.println(responseStatusCode);
+		System.out.println(time);
+		System.out.println(responseBody);
+		System.out.println(response.getHeader("content-type"));
+		
+		AssertJUnit.assertEquals(statusExpected, responseStatusCode);
+		
+	}	
+
+}
