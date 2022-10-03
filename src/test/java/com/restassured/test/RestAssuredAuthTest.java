0d5148422b73dae99a0c947ea2cea845d0fc33a9
Diego Montoya <diego.mtylop@gmail.com>
2022-10-03 17:09:22 -0500

    Renombrando clase de prueba y actualizando dependencias

--- src/test/java/com/restassured/test/RestAssuredAuthTest.java
+++ src/test/java/com/restassured/test/RestAssuredAuthTest.java
@@ -6,21 +6,22 @@ import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import static org.hamcrest.Matchers.*;
 
-public class RestAssureAuth extends BaseClassAuth{
+public class RestAssuredAuthTest extends BaseClassAuth{
 
+	/**
+	 * Anotaciones de allure usadas para dar información adicional de la prueba cuando
+	 * se genera el reporte
+	 */
 	@Test(description="Valid Authentication Scenario with valid username and password.")
 	@Severity(SeverityLevel.BLOCKER)
 	@Description("Test Description: Login test with valid username and password.")
 	@Story("Get authentication token")
 	@Step("GET request to the authentication")
 	public void test1() {
-		
 		RestAssured.given()
 			.get()
 			.then()
 			.statusCode(200)
 			.body("authenticated", equalTo(true));
-		
 	}
-
 }
