9d8163cdae0a0b6ea2b08cda01ac02d89bde3527
Diego Montoya <diego.mtylop@gmail.com>
2022-10-13 18:46:31 -0500

    Usando RestAssured.baseURI en lugar del import estático para mejor legibilidad. Eliminando imports no usados

--- src/test/java/com/restassured/test/GetAndPostExample.java
+++ src/test/java/com/restassured/test/GetAndPostExample.java
@@ -1,5 +1,6 @@
 package com.restassured.test;
 
+import io.restassured.RestAssured;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
@@ -8,15 +9,11 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class GetAndPostExample {
 	
 	@Test
 	public void testGet() {
-		
-		baseURI =  "https://reqres.in/api";
+		RestAssured.baseURI = "https://reqres.in/api";
 		
 		when().
 			get("/users?page=2").
@@ -24,18 +21,16 @@ public class GetAndPostExample {
 			statusCode(200).
 			body("data.size()", is(6)).
 			body("data.first_name", hasItems("George", "Rachel"));
-				
 	}
 	
 	@Test
 	public void testPost() {
-		
 		JSONObject request = new JSONObject();
 		
 		request.put("name", "Raghav");
 		request.put("job", "Teacher");
-		
-		baseURI =  "https://reqres.in/api";
+
+		RestAssured.baseURI =  "https://reqres.in/api";
 		
 		given().
 			header("Content-Type", "application/json").
@@ -46,7 +41,5 @@ public class GetAndPostExample {
 		then().
 			statusCode(201).
 			log().all();
-				
 	}
-
 }
